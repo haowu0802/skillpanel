57a087303aa1f1ea61afb5d6672108f44871c5a7
Hao Wu <haowu0802@gmail.com>
2017-07-29 16:37:44 -0700

    refine FT with User Story

--- skilllist/functional_tests.py
+++ skilllist/functional_tests.py
@@ -1,12 +1,24 @@
-
+"""
+The functional tests / black box tests / acceptance tests / end2end tests
+    for Skill List
+        by Leon (Hao Wu)
+"""
 from selenium import webdriver  # the web driver for functional tests
+
+"""conf"""
 
 # local url of web app
 localurl = 'http://127.0.0.1:8000'
 
 # init browser
 browser = webdriver.Firefox()  # use firefox for testing
+
+""" user story """
+# My lady comes to the cool website that helps people saving their skills
 browser.get(localurl)
 
-# check if Django is there in the title
-assert 'Django' in browser.title
+# She sees in the title, "Skill List", which is what the web app does.
+assert 'Skill List' in browser.title
+
+# She's satisfied and left.
+browser.quit()
