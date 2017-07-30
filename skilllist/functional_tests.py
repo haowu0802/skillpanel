fee2eef04a429f0a99142c036307e141b23f1d13
Hao Wu <haowu0802@gmail.com>
2017-07-29 17:49:43 -0700

    add unittest and refactor tests

--- skilllist/functional_tests.py
+++ skilllist/functional_tests.py
@@ -4,21 +4,33 @@ The functional tests / black box tests / acceptance tests / end2end tests
         by Leon (Hao Wu)
 """
 from selenium import webdriver  # the web driver for functional tests
+import unittest  # use unittest module for whilte box tests
+
 
 """conf"""
-
 # local url of web app
 localurl = 'http://127.0.0.1:8000'
 
-# init browser
-browser = webdriver.Firefox()  # use firefox for testing
 
-""" user story """
-# My lady comes to the cool website that helps people saving their skills
-browser.get(localurl)
+class NewVisitorTest(unittest.TestCase):  # group tests into classes
+    """a new visitor's common behavior first time to the web app"""
+    def setUp(self):
+        """setUp runs before each test"""
+        self.browser = webdriver.Firefox()
 
-# She sees in the title, "Skill List", which is what the web app does.
-assert 'Skill List' in browser.title
+    def tearDown(self):
+        """tearDown runs after each test, even with exceptions"""
+        self.browser.quit()
 
-# She's satisfied and left.
-browser.quit()
+    def test_can_visit_the_page_and_leave(self):
+        # My lady comes to the cool website that helps people saving their skills
+        self.browser.get(localurl)
+        # She sees in the title, "Skill List", which is what the web app does.
+        self.assertIn('Skill List', self.browser.title)
+        self.fail('todo')
+        # She's satisfied and left.
+
+
+"""main"""
+if __name__ == '__main__':
+    unittest.main()  # main runs the test runner which auto find all test_ functions and run them
