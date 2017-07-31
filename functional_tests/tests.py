7405e88dcdc5da189f93d98d629f0de4ec149bea
Hao Wu <haowu0802@gmail.com>
2017-07-31 10:01:29 -0700

    refactor wait helper

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,14 +6,16 @@ The functional tests / black box tests / acceptance tests / end2end tests
 from django.test import LiveServerTestCase
 from selenium import webdriver  # the web driver for functional tests
 from selenium.webdriver.common.keys import Keys  # for sending key press to webdriver
+from selenium.common.exceptions import WebDriverException
 import time  # to use sleep
 
 
 """conf"""
 URL_LOCAL = 'http://127.0.0.1:8000'  # local url of web app
 WAIT_SEC = 3  # number of seconds wait at start
-SHORT_PAUSE = 1  # generic short wait, for latency, slow machines
+SHORT_PAUSE = 0.5  # generic short wait, for latency, slow machines
 LONG_PAUSE = 10  # generic long pause, for debugging
+MAX_WAIT = 10  # wait no longer than this
 
 
 class NewVisitorTest(LiveServerTestCase):  # group tests into classes
@@ -27,15 +29,24 @@ class NewVisitorTest(LiveServerTestCase):  # group tests into classes
         """tearDown runs after each test, even with exceptions"""
         self.browser.quit()
 
-    def check_for_text_in_rows_of_table(self, row_text):
+    def wait_for_text_in_rows_of_table(self, row_text):
         """
-        helper for check if row_text is in table
+        wait for table to load and check for existence of row_text in table
+            SHORT_PAUSE between reties
         :param row_text:
         :return:
         """
-        table = self.browser.find_element_by_id('id_log_table')  # find the logs table by id
-        rows = table.find_elements_by_tag_name('tr')  # get all table rows from table
-        self.assertIn(row_text, [row.text for row in rows])
+        start_time = time.time()  # timer for MAX WAIT
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_log_table')  # find the logs table by id
+                rows = table.find_elements_by_tag_name('tr')  # get all table rows from table
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(SHORT_PAUSE)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Arya Stark comes to the cool website that helps people saving their skills
@@ -61,18 +72,16 @@ class NewVisitorTest(LiveServerTestCase):  # group tests into classes
         # When she press enter, the page updates, and lists "1. Poke the training dummy with needle."
         #   as an item to the skill log
         input_box.send_keys(Keys.ENTER)
-        time.sleep(SHORT_PAUSE)  # wait for enter to work, db to finish updating, page to update
-        self.check_for_text_in_rows_of_table('1. Poke the training dummy with needle.')
+        self.wait_for_text_in_rows_of_table('1. Poke the training dummy with needle.')
 
         # There's still a text box inviting her to add another log, she enters "Chase the cat in the dungeon" (Sneaking)
         input_box = self.browser.find_element_by_id('id_new_log')  # re-assign the new input box to input_box
         input_box.send_keys('Chase the cat in the dungeon.')  # a different log
         input_box.send_keys(Keys.ENTER)
-        time.sleep(SHORT_PAUSE)
 
         # The page updates again, now showing two logs
-        self.check_for_text_in_rows_of_table('1. Poke the training dummy with needle.')
-        self.check_for_text_in_rows_of_table('2. Chase the cat in the dungeon.')
+        self.wait_for_text_in_rows_of_table('1. Poke the training dummy with needle.')
+        self.wait_for_text_in_rows_of_table('2. Chase the cat in the dungeon.')
 
         # Arya wonders if the site will remember her lists, and sees that a URL is generated, with some text
         self.fail('listing first log finished, more to do...')
