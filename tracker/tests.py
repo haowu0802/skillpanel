491b64ed625290e814a3a8daa8193329418caaff
Hao Wu <haowu0802@gmail.com>
2017-07-30 18:07:11 -0700

    add Log model, migrations, and unittests for it

--- tracker/tests.py
+++ tracker/tests.py
@@ -1,6 +1,13 @@
+"""
+The unit tests
+    for Skill Tracker
+        by Leon (Hao Wu)
+"""
 from django.test import TestCase  # extends unittest.TestCase
 from django.core.urlresolvers import resolve  # for resolving url internally
 from django.http import HttpRequest  # for request pages
+
+from tracker.models import Log
 
 from tracker.views import home_page  # for testing home_page view
 # Create your tests here.
@@ -22,3 +29,25 @@ class HomePageTest(TestCase):
         self.assertIn('A new log item', response.content.decode())
         # check again if using the right tpl
         self.assertTemplateUsed(response, 'home.html')
+
+
+class LogModelTest(TestCase):
+
+    def test_saving_and_retrieving_logs(self):
+        # create and save 1st log
+        first_log = Log()
+        first_log.text = 'The first (ever) log item'
+        first_log.save()
+        # create and save 2nd log
+        second_log = Log()
+        second_log.text = 'Log the second'
+        second_log.save()
+        # get all 2 logs and see if we have 2
+        saved_logs = Log.objects.all()
+        self.assertEqual(saved_logs.count(), 2)
+        # specifically check each log
+        first_saved_log = saved_logs[0]
+        second_saved_log = saved_logs[1]
+        # to see if they have the correct content
+        self.assertEqual(first_saved_log.text, 'The first (ever) log item')
+        self.assertEqual(second_saved_log.text, 'Log the second')
