ebb98e5be12e3157ed48962f56bf59368ac17bdd
Hao Wu <haowu0802@gmail.com>
2017-08-12 13:01:06 -0700

    allowing all traffic

--- skilllist/settings.py
+++ skilllist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ph-4twliqe=t=(az!ph)$b87bgj2a%gcv&rj^x#jq)y*@s*s@v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
