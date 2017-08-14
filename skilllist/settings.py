0e9295a9c0a01fce3a24bce27af5ef7e9303e4dc
Hao Wu <haowu0802@gmail.com>
2017-08-14 14:05:17 -0700

    switch to postgresql

--- skilllist/settings.py
+++ skilllist/settings.py
@@ -77,8 +77,8 @@ WSGI_APPLICATION = 'skilllist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, './database/db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'skillpanel',
     }
 }
 
