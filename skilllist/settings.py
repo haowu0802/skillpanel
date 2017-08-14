0c4524e88518560986e9ac057f1e8c2224c385cf
Hao Wu <haowu0802@gmail.com>
2017-08-14 14:47:30 -0700

    config static files and whitenoise

--- skilllist/settings.py
+++ skilllist/settings.py
@@ -122,8 +122,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # collectstatic target location, ignored in Git
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, './staticfiles'))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'  # whitenoise used for static res on Heroku
